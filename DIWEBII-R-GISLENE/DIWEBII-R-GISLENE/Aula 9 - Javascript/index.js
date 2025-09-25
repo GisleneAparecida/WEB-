8714ac4f16d2adc66be724a1744aff93bbf1ee23
GisleneAparecida <gislene.eat@gmail.com>
2025-09-25 20:58:12 -0300

    Add files via upload

---
+++ DIWEBII-R-GISLENE/DIWEBII-R-GISLENE/Aula 9 - Javascript/index.js
@@ -0,0 +1,59 @@
+// console.log('Gislene Aparecida Oliveira Silva');
+// console.log('Gislene "Aparecida" "Oliveira" Silva');
+// console.log( ´Gislene "Aparecida" 'Oliveira' Silva´);
+// console.log(123456789);
+
+// console.log(´Meu nome é "Gislene", estou aprendendo javascript agora, e são exatamente 20:09, 'da noite');
+
+// tipo de variaeis 
+// var - está em desuso
+let nome = "João";
+let nome1 = "Maria";
+let nome3 = "Eduardo";
+
+let data1 = 1984;
+let data2 = 2000;
+let data3 = 2012;
+let data4 = 2015;
+
+console.log(`${nome}, nasceu em ${data1} .`);
+console.log(`Em ${data2} , ${nome} conheceu ${nome1} .`);
+console.log(`${nome} casou-se com ${nome} em ${data3} .`);
+console.log(`${nome1} teve 1 filho com ${nome} em ${data4} .`);
+console.log(`o filho de ${nome} se chama  ${nome2} .`);
+
+
+
+// tipos de dados
+// string - são um conjunto de caracteres que formam palavras que formam textos
+// number - valores numéricos / numeros inteiros ou pontos flutuantes com casas decimais.
+// boolean - representação lógica - verdadeiro ou falso
+// const valor = false; true or false
+// BigInt - representa números inteiros com valores muito altos
+// undefined - um valor na variável que não foi declaraddo 
+// let valor;
+// null = representa uma variável que não tem valor ou que foi exvasiada
+
+// (Gislen Aparecida Oliveira Silva tem xx AnalyserNode,)
+// (pesa 75kg, tem 4,65 de altura e seu)
+// (IMC é de ...)
+// (Gislen Aparecida nasceu em ----)
+
+const nome = 'Gislene Aparecida';
+const sobreNome = 'Oliveira Silva';
+const idade = xx;
+const peso = 75;
+const altura = 1.75;
+let imc;// peso / (altura *altura>)
+let dataNascimento;
+
+imc= peso / (altura * altura);
+console.log(imc);
+
+imc = peso / (altura * altura);
+console.log(imc);
+dataNascimento = 2025 - idade
+console.log(dataNascimento);
+
+console.log(`${nome} #{sobrenome} tem ${idade}  anos `);
+console.log(`e pesa ${peso} kg`);
